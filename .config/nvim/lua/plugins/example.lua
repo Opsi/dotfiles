e376535382b05be4ce9d975b5b5575b84259e6bc
Robin Wiersig <Opsi@users.noreply.github.com>
2025-05-20 21:05:16 +0200

    update

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -20,7 +20,8 @@ return {
     },
   },
 
-  -- change trouble config
+  {
+    -- change trouble config
     "folke/trouble.nvim",
     -- opts will be merged with the parent spec
     opts = { use_diagnostic_signs = true },
