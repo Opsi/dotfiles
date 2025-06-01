da98a161a7aea1eeeb99afa7fc3b5a193834ffdd
Robin Wiersig <Opsi@users.noreply.github.com>
2025-06-01 15:32:36 +0200

    add nmtui integration to waybar

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -689,6 +689,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
+        biome = {},
         --
 
         lua_ls = {
