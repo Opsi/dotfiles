4c0a7571c63027fe3909c4f1936ca4946c8d1b00
Robin Wiersig <Opsi@users.noreply.github.com>
2025-06-16 08:49:45 +0200

    ts_ls did not work with node workspaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -711,8 +711,6 @@ require('lazy').setup({
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
         biome = {},
         emmet_language_server = {
           filetypes = { 'css', 'html', 'javascriptreact', 'typescriptreact' },
