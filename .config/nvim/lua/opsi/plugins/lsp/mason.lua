e23ad1afe9e7ee6f3506f72fef8c947d95c5e3d8
Robin Wiersig <Opsi@users.noreply.github.com>
2025-06-18 15:54:48 +0200

    mason and lsp wip ... also surrond

---
+++ .config/nvim/lua/opsi/plugins/lsp/mason.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    'mason-org/mason.nvim',
+    dependencies = {
+      'mason-org/mason-lspconfig.nvim',
+    },
+    config = function()
+      local mason = require 'mason'
+      local mason_lspconfig = require 'mason-lspconfig'
+
+      mason.setup {
+        ui = {
+          icons = {
+            package_installed = '✓',
+            package_pending = '➜',
+            package_uninstalled = '✗',
+          },
+        },
+      }
+
+      mason_lspconfig.setup {
+        ensure_installed = {
+          'bashls',
+          'dockerls',
+          'yamlls',
+          'astro-language-server',
+          'basedpyright',
+          'biome',
+          'emmet-language-server',
+          'gdtoolkit',
+          'gopls',
+          'lua-language-server',
+          'node-debug2-adapter',
+          'oxlint',
+          'pyproject-fmt',
+          'ruff',
+          'rust-analyzer',
+          'stylua',
+          'tailwindcss-language-server',
+        },
+      }
+    end,
+  },
+}
